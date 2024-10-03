185e79e6153d7678f6f6fa75a8f2c0c60ce2f0e7
Quramy <yosuke.kurami@gmail.com>
2024-10-03 15:47:16 +0900

    fix: Make Query.posts as strict

--- src/server/graphql/types.ts
+++ src/server/graphql/types.ts
@@ -2,6 +2,9 @@ import type { PrismaClient } from "@prisma/client";
 import type { YogaInitialContext } from "graphql-yoga";
 import { Loaders } from "./loaders/index.js";
 
+export type { Post as PostParent } from "@prisma/client";
+export type { Comment as CommentParent } from "@prisma/client";
+
 export type BaseContext = {
   prisma: PrismaClient;
   loaders: Loaders;
