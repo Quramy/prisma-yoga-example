6e15c4b884c1feebbb9011df8a61f6fd1aa3684c
Quramy <yosuke.kurami@gmail.com>
2024-10-12 01:13:55 +0900

    chore: Modify schema

--- src/server/graphql/typeDefs.ts
+++ src/server/graphql/typeDefs.ts
@@ -3,11 +3,19 @@ export const typeDefs = /* GraphQL */ `
     id: ID!
   }
 
+  type PageInfo {
+    hasNextPage: Boolean!
+    hasPreviousPage: Boolean!
+    startCursor: String
+    endCursor: String
+    total: Int
+  }
+
   type Post implements Node {
     id: ID!
     title: String!
     body: String!
-    comments: [Comment!]!
+    comments: CommentConnection
   }
 
   type Comment implements Node {
@@ -15,10 +23,41 @@ export const typeDefs = /* GraphQL */ `
     body: String!
   }
 
+  type CommentEdge {
+    cursor: String!
+    node: Comment!
+  }
+
+  type CommentConnection {
+    edges: [CommentEdge!]!
+    nodes: [Comment!]!
+    pageInfo: PageInfo!
+  }
+
+  input AddCommentToPostInput {
+    clientMutationId: ID
+    body: String!
+  }
+
+  type AddCommentToPostResultSuccess {
+    commentEdge: CommentEdge!
+    commentNode: Comment!
+  }
+
+  type MutationFailure {
+    reason: String
+  }
+
+  union AddCommentToPostResult = AddCommentToPostResultSuccess | MutationFailure
+
   type Query {
     posts: [Post!]!
     post(id: ID!): Post
     comment(id: ID!): Comment
     node(id: ID!): Node
+  }
+
+  type Mutation {
+    addCommentToPost(input: AddCommentToPostInput): AddCommentToPostResult!
   }
 `;
